cc19d6a134c682ff2d4037fd03e516d9a166b62c
LRZ <1015450627@qq.com>
2023-06-16 17:25:30 +0800

    6.16 Nacos 服务注册

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/lrz/springcloud/OrderNacosMain833.java
@@ -0,0 +1,13 @@
+package com.lrz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain833 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain833.class,args);
+    }
+}
